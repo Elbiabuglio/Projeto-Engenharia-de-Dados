acc94828eefcdfe6a6dd52dd528f8e6f178986b6
Elbiabuglio <elbiasimone@hotmail.com>
2025-01-29 22:35:52 -0300

    Conexão PostgreSQL com Python

---
+++ 01. Postgree/3.Referencias/Ambiente Virtual.py
@@ -0,0 +1,9 @@
+#Configuracao
+##pip install virtualenv
+##ou pip3 install virtualenv
+
+python -m venv  sgbds
+#sgbds\Scripts\activate
+#C:\Users\Note reserva\Desktop\Curso Engenharia Dados\Python - SGBDS\sgbds\Scripts\activate
+#se der erro ao criar ou ativar rodar comando abaixo no Shel como ADm para iber
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
